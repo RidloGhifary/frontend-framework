93b0f3dd4b13428d8ef67ad08ca8502d030d3c41
RidloGhifary <ridloghfry@gmail.com>
2024-07-17 16:58:06 +0700

    feat: implement state

--- src/user.js
+++ src/user.js
@@ -1,5 +1,16 @@
+import { createComponent } from "../framework";
 import { div } from "../framework/element";
 import { onClick } from "../framework/event";
 
-export const User = ({ firstName, lastName }) =>
-  div`${onClick(() => alert(firstName))} Hello ${firstName} ${lastName}!`;
+const methods = {
+  changeName: (state, firstName) => ({ ...state, firstName }),
+};
+
+const initialState = { firstName: "Ridlo", lastName: "Ghifary" };
+
+const template = ({ firstName, lastName, methods }) =>
+  div`${onClick(() =>
+    methods.changeName("Achmad")
+  )} Hello ${firstName} ${lastName}`;
+
+export const User = createComponent({ template, methods, initialState });
