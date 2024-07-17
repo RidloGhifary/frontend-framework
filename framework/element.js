94e59834ca5ca754a679c30f9a1d21c94b82759e
RidloGhifary <ridloghfry@gmail.com>
2024-07-17 16:27:48 +0700

    feat: adding virtual DOM snabbdom

--- framework/element.js
+++ framework/element.js
@@ -1,12 +1,37 @@
+import { h } from "snabbdom/h";
+
+const initialState = {
+  template: "",
+  on: {},
+};
+
+const createReducer = (args) => (acc, currentString, index) => {
+  const currentArg = args[index];
+
+  /**
+   * Here, we define the behavior of an event node and this
+   * is where the type is important :D
+   */
+  if (currentArg && currentArg.type === "event") {
+    return { ...acc, on: { click: currentArg.click } };
+  }
+
+  return {
+    ...acc,
+    template: acc.template + currentString + (args[index] || ""),
+  };
+};
+
 const createElement =
   (tagName) =>
-  (strings, ...args) => ({
-    type: tagName,
-    template: strings.reduce(
-      (acc, currentString, index) => acc + currentString + (args[index] || ""),
-      ""
-    ),
-  });
+  (strings, ...args) => {
+    const { template, on } = strings.reduce(createReducer(args), initialState);
+
+    return {
+      type: "element",
+      template: h(tagName, { on }, template),
+    };
+  };
 
 export const div = createElement("div");
 export const p = createElement("p");
--- framework/index.js
+++ framework/index.js
@@ -1,8 +1,10 @@
+import * as snabbdom from "snabbdom";
+
+const patch = snabbdom.init([
+  require("snabbdom/modules/eventlisteners").default,
+]);
+
 export const init = (selector, component) => {
   const app = document.querySelector(selector);
-  const newElement = document.createElement(component.type);
-  const newTextContent = document.createTextNode(component.template);
-
-  newElement.append(newTextContent);
-  app.append(newElement);
+  patch(app, component.template);
 };
